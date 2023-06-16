6656ed1867df8614a693dd24d1fa617c787e4a23
jamespwolpert <jamespwolpert@gmail.com>
2023-06-16 11:16:00 -0400

    nothing different

--- plasmic-init.ts
+++ plasmic-init.ts
@@ -4,7 +4,7 @@ export const PLASMIC = initPlasmicLoader({
     {
       id: "eYrbo7rex7kTK7jfgmqGEH",  // ID of a project you are using
       token: "VWs0I0w2H0pIm9ykD26xY3PkpR5WuTv41pIPP2eIxOJbwd2TzV06XYL3rMJ264RPCejQXaNF32J8W7yRig",  // API token for that project
-      version: 'production'
+      // version: 'production'
       // version:
       //   process.env.NODE_ENV === 'production'
       //     ? 'prod'
